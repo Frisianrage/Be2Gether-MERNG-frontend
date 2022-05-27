a184a20c35f1c98bf18a961051467b894695b3d4
Frisianrage <65295703+Frisianrage@users.noreply.github.com>
2022-05-26 18:31:30 -0700

    deleted useEffect from AuthNavBar

--- src/components/navbar/AuthNavBar.js
+++ src/components/navbar/AuthNavBar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import {useQuery} from '@apollo/client'
 import { Link } from 'react-router-dom';
 import {AuthContext} from '../../context/auth'
